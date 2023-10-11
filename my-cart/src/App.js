8f219bc46882a43ec5dced0ce19513984b3a1de2
Santy <santhoshsurya195@gmail.com>
2023-10-11 22:34:50 +0530

    Added product page

--- my-cart/src/App.js
+++ my-cart/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/products" element={<Products />}></Route>
-          <Route path="/products/id" element={<ProductDetails />}></Route>
+          <Route path="/products/:id" element={<ProductDetails />}></Route>
         </Routes>
       </div>
     </>
